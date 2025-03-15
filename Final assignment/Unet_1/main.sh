fb78cdb4241547cd0f423cc10d32e1c0cbd3af0e
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-15 14:54:19 +0100

    add cos adp to unet1

--- Final assignment/Unet_1/main.sh
+++ Final assignment/Unet_1/main.sh
@@ -4,7 +4,7 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 75 \
-    --lr 0.001 \
+    --lr 0.005 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "unet-training" \

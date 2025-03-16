23e49f19b330e00911c75e663933ebb959b2290c
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-16 23:32:34 +0100

    lr = 0.001, epoch =30 scheduler

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train_1.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 10 \
-    --lr 0.01 \
+    --epochs 30 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet_1 lr 0.01 epoch10 without cos" \
+    --experiment-id "unet_1 lr 0.001 epoch30 ReduceLROnPlateau" \

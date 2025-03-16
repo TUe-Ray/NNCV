7c260aa7975f7ec3ccdb7822ba06e4e64d65b7dd
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-16 23:52:08 +0100

    512512

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -7,4 +7,4 @@ python3 train_1.py \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet_1 lr 0.001 epoch30 ReduceLROnPlateau" \
+    --experiment-id "unet_1 lr 0.001 epoch30 ReduceLR " \

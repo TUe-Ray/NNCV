81f3b4f337b01c420529ad4479df24d8b40d5955
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-16 23:57:01 +0100

    train_resnet34

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_1.py \
+python3 train_resnet34.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 30 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet_1 lr 0.001 epoch30 ReduceLR " \
+    --experiment-id "resnet34 lr 0.001 epoch30 256" \

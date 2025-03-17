f5152599375b2623da9fa3468c0bde11a281823e
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-17 18:25:29 +0100

    no lr for resnet

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train_resnet34.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 40 \
+    --epochs 30 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "resnet34+unet 64/40/0.001" \
+    --experiment-id "resnet34+unet 64/30/0.001 no lr for resnet" \

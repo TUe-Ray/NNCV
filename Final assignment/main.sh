32aa3e1a9df083a78034f9776350d2be8f8a3f60
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-17 01:21:25 +0100

    unet1 256 three channel normalize

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train_1.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 5 \
+    --epochs 10 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet1 512 epoch 5" \
+    --experiment-id "unet1 256 three channel normalization" \
--- Final assignment/train_1.py
+++ Final assignment/train_1.py
@@ -93,9 +93,9 @@ def main(args):
     # Define the transforms to apply to the data
     transform = Compose([
         ToImage(),
-        Resize((512, 512)),
+        Resize((256, 256)),
         ToDtype(torch.float32, scale=True),
-        Normalize((0.5,), (0.5,)),
+        Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
 
     # Load the dataset and make a split for training and validation

8aa6fab5b2feef744f7be6d240b753128a923ac7
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-17 01:08:33 +0100

    unet 512 epoch5

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_resnet34.py \
+python3 train_1.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 30 \
+    --epochs 5 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "resnet_unet34 lr 0.001 epoch30 256" \
+    --experiment-id "unet1 512 epoch 5" \

cf30d8b3c1bf919014100753023aa8edde2d8f89
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-17 02:56:03 +0100

    batch 16

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train_1.py \
     --data-dir ./data/cityscapes \
-    --batch-size 64 \
+    --batch-size 16 \
     --epochs 10 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet1 512 lr0.001 epoch 10 /3C normalization" \
+    --experiment-id "unet1 512 lr0.001 batch 16 epoch 10 /3C normalization" \

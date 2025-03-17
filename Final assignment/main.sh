4e984a17379e1a798c93b37ea07d61357f4b5a44
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-17 03:06:14 +0100

    resnet34+unet 64/10/0.001

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_1.py \
+python3 resnet34_unet.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 64 \
     --epochs 10 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "unet1 512 lr0.001 batch 16 epoch 10 /3C normalization" \
+    --experiment-id "resnet34+unet 64/10/0.001" \

20665ec56b498d8f3dfa80189aba54f52de85e0f
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-17 18:08:01 +0100

    resnet34 training 40 epoch

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train_resnet34.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 10 \
+    --epochs 40 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "resnet34+unet 64/10/0.001" \
+    --experiment-id "resnet34+unet 64/40/0.001" \

10ab51f4e9dd5dfaa9281d31eb200823bc3c7092
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-18 17:18:03 +0100

    101 augumentation

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_ResUnet101_sqscheduler.py \
+python3 train_ResUnet101_augumentation.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 30 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "ResUnet101 64/50/0.001  ChangingScheduler" \
+    --experiment-id "ResUnet101 64/30/0.001 Augumentation" \

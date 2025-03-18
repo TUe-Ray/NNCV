1a897c356509f2e2ae4ecc39fa9ab79bd48ff18c
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-18 16:49:20 +0100

    101 bug fix

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,6 +1,6 @@
 wandb login
 
-python3 train_ResUnet101_Changingscheduler.py \
+python3 train_ResUnet101_sqscheduler.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 30 \

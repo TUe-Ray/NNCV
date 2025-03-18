838c7097c1a036b1cc4bddf0f23219ca04e4e6cb
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-18 02:42:59 +0100

    101 sqscheduler

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_ResUnet101.py \
+python3 train_ResUnet101_sqscheduler.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
-    --epochs 30 \
+    --epochs 50 \
     --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "ResUnet101 64/30/0.001  lr*0.1 for resnet101" \
+    --experiment-id "ResUnet101 64/50/0.001  SequentialScheduler" \

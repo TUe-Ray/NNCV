78bbb0754dcf0fee921ab7ef32db6c529bf6b9f7
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-21 00:44:11 +0100

    lr 0.0001 30 epochs

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train_ResUnet101_augumentation_512.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
     --epochs 30 \
-    --lr 0.0005 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "ResUnet101 512 16/30/0.0005 aug" \
+    --experiment-id "ResUnet101 512 16/30/0.0001 aug" \

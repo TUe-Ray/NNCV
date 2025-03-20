fcf9b023cbed7256f53619a21e38fdab9d0e898a
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-21 00:23:45 +0100

    resunet 101 512 aug

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_ResUnet101_augumentation.py \
+python3 train_ResUnet101_augumentation_512.py \
     --data-dir ./data/cityscapes \
-    --batch-size 64 \
-    --epochs 40 \
-    --lr 0.001 \
+    --batch-size 32 \
+    --epochs 10 \
+    --lr 0.0005 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "ResUnet101 64/40/0.001 Aug-2" \
+    --experiment-id "ResUnet101 512 32/10/0.0005 aug" \

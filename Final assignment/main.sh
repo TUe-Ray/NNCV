2a11606b2b0a4f039f9e6ae24a26c336e286ca20
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-25 18:24:55 +0100

    dice evaluation bug fix test

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train_DiceCE_ResUnet101_aug_512.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
-    --epochs 10 \
+    --epochs 2 \
     --lr 0.0005 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "DiceCE ResUnet101 512 16/10/0.0005 aug" \
+    --experiment-id "test" \

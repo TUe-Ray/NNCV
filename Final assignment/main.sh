98110098b07dd9008730fb949da1a1edf4477bd4
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-31 16:24:03 +0200

    Sota

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 train_FocalDice_ResUnet101_aug_512.py \
+python3 SOTA_Dice_aug_512.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
     --epochs 30 \
-    --lr 0.0005 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "ResUnet101 FocalDice0.5_LROnPla 16/30/5e-4 aug" \
+    --experiment-id "DeepLab+ 16/30/1e-4 aug" \

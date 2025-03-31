cc3b18baa737f554188de9fff9cda6757491ef7d
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-03-31 02:19:34 +0200

    WeightDice bug fix

--- Final assignment/WeightedDiceLoss.py
+++ Final assignment/WeightedDiceLoss.py
@@ -3,8 +3,9 @@ from typing import Optional, List
 import torch
 import torch.nn.functional as F
 from torch.nn.modules.loss import _Loss
-from ._functional import soft_dice_score, to_tensor
-from .constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
+from segmentation_models_pytorch.losses._functional import soft_dice_score, to_tensor
+
+from segmentation_models_pytorch.losses.constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
 
 __all__ = ["WeightedDiceLoss"]
 

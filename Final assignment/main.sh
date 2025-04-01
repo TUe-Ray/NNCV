389ac68845f6e94894fab2581095077052e0b2a3
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-01 19:07:19 +0200

    fintunred mean

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train_Segformer_Finetuned.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 8 \
     --epochs 30 \
-    --lr 0.00001 \
+    --lr 0.000005 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "Finetuned_Segformerb5 finetuned_mean 16/30/1e-5 aug" \
+    --experiment-id "Finetuned_Segformerb5 finetuned_mean 16/30/5e-6 aug" \

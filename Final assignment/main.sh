c4d0fb62ced8c6a33030f45f6b4adf46e8d69c4b
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-10 17:16:33 +0200

    segformer 1024

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -1,10 +1,10 @@
 wandb login
 
-python3 test_load_model.py \
+python3 train_Segformer_Finetuned.py \
     --data-dir ./data/cityscapes \
     --batch-size 8 \
-    --epochs 2 \
+    --epochs 30 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "load model" \
+    --experiment-id "FinTun_Segforb5 1024 FT_mean 8/30/1e-4_1e-6 aug" \

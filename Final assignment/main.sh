228849d59cbcd165c2ffe3fc11fdb2a3b5e2efaf
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-10 20:21:56 +0200

    segformer Wdice 8

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train_Weight_Dice_Segformer_FT.py \
     --data-dir ./data/cityscapes \
-    --batch-size 16 \
+    --batch-size 8 \
     --epochs 30 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "FT_SegForB5 WeightDice_512 FT_mean 16/30/1e-4_1e-6 aug" \
+    --experiment-id "FT_SegForB5 WeightDice_512 FT_mean 8/30/1e-4_1e-6 aug" \

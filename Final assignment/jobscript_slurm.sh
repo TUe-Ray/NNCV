e832922683c5d3cf60d752628aa09083c11dd4bf
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-18 01:06:41 +0200

    auto cast

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
-#SBATCH --time=01:30:00
+#SBATCH --time=03:00:00
 
 
 srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,8 +3,8 @@ wandb login
 python3 train_Segformer_Finetuned_copy.py \
     --data-dir ./data/cityscapes \
     --batch-size 2 \
-    --epochs 2 \
+    --epochs 20 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "TEST_audocast" \
+    --experiment-id "TEST_FT_SegForB5 1024 FT_mean 2/2/1e-4_1e-7 aug" \

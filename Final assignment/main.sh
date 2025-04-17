3757ef9873115663bfd2771ac653e33f46b9f913
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-17 23:43:56 +0200

    tes

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train_Segformer_Finetuned_copy.py \
     --data-dir ./data/cityscapes \
-    --batch-size 4 \
+    --batch-size 2 \
     --epochs 2 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "TEST_FT_SegForB5 1024 FT_mean 4/2/1e-4_1e-7 aug" \
+    --experiment-id "TEST_FT_SegForB5 1024 FT_mean 2/2/1e-4_1e-7 aug" \

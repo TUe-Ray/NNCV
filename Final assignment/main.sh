a81dfbc572ce05a12c5188a3ed7a94e76ce04076
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-18 23:34:15 +0200

    raw seg

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -7,4 +7,4 @@ python3 train_Segformer_Raw.py \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id " Raw_SegForB5 512FT_mean 8/20/1e-4_1e-7 aug" \
+    --experiment-id " Raw_SegForB5 512FT_mean 8/20/1e-4_1e-7 Noaug" \

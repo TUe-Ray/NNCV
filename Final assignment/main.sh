f9e48b8ad0338dc23fae06005e8e931e069afcbb
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-18 23:46:33 +0200

    raw seg

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train_Segformer_Raw.py \
     --data-dir ./data/cityscapes \
     --batch-size 8 \
     --epochs 20 \
-    --lr 0.0001 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id " Raw_SegForB5 512FT_mean 8/20/1e-4_1e-7 Noaug" \
+    --experiment-id " Raw_SegForB5 512FT_mean 8/20/1e-3_1e-7 Noaug" \

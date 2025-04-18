6f52ab5a0e06d08db0b039852437ad65c4bc6161
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-18 18:00:40 +0200

    512

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 ATrain_Segformer_Finetuned.py \
     --data-dir ./data/cityscapes \
-    --batch-size 2 \
+    --batch-size 8 \
     --epochs 20 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "FT_SegForB5 1024Dice FT_mean 2/20/1e-4_1e-7 aug" \
+    --experiment-id "FT_SegForB5 512Dice FT_mean 8/20/1e-4_1e-7 aug" \

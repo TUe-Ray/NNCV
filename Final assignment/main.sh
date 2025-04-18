cd4a165aeb9b7bb428aab439946ec3db9a037e60
Shao-Ruei Huang <s.huang5@student.tue.nl>
2025-04-19 00:00:17 +0200

    flip crop rotate FTB5

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train_Segformer_Finetuned_copy.py \
     --data-dir ./data/cityscapes \
     --batch-size 8 \
     --epochs 20 \
-    --lr 0.001 \
+    --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id " flipCropRotate FT_SegForB5 512FT_mean 8/20/1e-3_1e-7 Noaug" \
+    --experiment-id " flipCropRotate FT_SegForB5 512FT_mean 8/20/1e-4_1e-7 Noaug" \
